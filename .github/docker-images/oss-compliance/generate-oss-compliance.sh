d1cf4563896951da2ed4bcb447ab5f650f5d80af
Roger Zhong <rgz@amazon.com>
2023-02-21 17:11:06 -0800

    oss compliance script

--- .github/docker-images/oss-compliance/generate-oss-compliance.sh
+++ .github/docker-images/oss-compliance/generate-oss-compliance.sh
@@ -5,30 +5,23 @@
 
 PRETTY_NAME=$(cat /etc/os-release | grep PRETTY_NAME)
 
-HOME_DIR=$1
+HOME_DIR=$(pwd)
 
 export HOME_DIR=${HOME_DIR}
-LINUX_PACKAGES=${HOME_DIR}/oss-compliance/linux-packages
-BUILD_FROM_SOURCE_PACKAGES_LICENCES=${HOME_DIR}/oss-compliance/build-from-source-packages/build-from-source-package-licenses.txt
+LINUX_PACKAGES=${HOME_DIR}/linux-packages
 
 set -e
 
 chmod +x ${LINUX_PACKAGES}/yum-packages.sh
-chmod +x ${LINUX_PACKAGES}/dkpg-packages.sh
+chmod +x ${LINUX_PACKAGES}/dpkg-packages.sh
 
-if [[ $PRETTY_NAME == *"Ubuntu"* ]]; then
-    ${LINUX_PACKAGES}/dkpg-packages.sh
+if [[ $PRETTY_NAME == *"Ubuntu"* || $PRETTY_NAME == *"Debian"* ]]; then
+    ${LINUX_PACKAGES}/dpkg-packages.sh
 fi
 
-if [[ $PRETTY_NAME == *"Amazon Linux"* ]]; then
+if [[ $PRETTY_NAME == *"Amazon Linux"* || $PRETTY_NAME == *"Red Hat Enterprise Linux"* || $PRETTY_NAME == "Fedora" ]]; then
   ${LINUX_PACKAGES}/yum-packages.sh
 fi
 
-if [[ $PRETTY_NAME == *"Red Hat Enterprise Linux"* ]]; then
-  ${LINUX_PACKAGES}/yum-packages.sh
-  BUILD_FROM_SOURCE_PACKAGES_LICENCES=${HOME_DIR}/oss-compliance/build-from-source-packages/build-from-source-package-licenses-ubi8.txt
-fi
-
-cp ${BUILD_FROM_SOURCE_PACKAGES_LICENCES} ${HOME_DIR}/BUILD_FROM_SOURCE_PACKAGES_LICENCES
-chmod +x ${HOME_DIR}/oss-compliance/test/test-oss-compliance.sh
-bash ${HOME_DIR}/oss-compliance/test/test-oss-compliance.sh ${HOME_DIR}
+chmod +x ${HOME_DIR}/test/test-oss-compliance.sh
+bash ${HOME_DIR}/test/test-oss-compliance.sh ${HOME_DIR}
